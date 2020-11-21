be0eeda9c536c82cbd0dbf21c18791b20b590de0
Lev <tikhonov.lev0@gmail.com>
2020-11-21 22:56:28 +0300

    fix

--- files/mess_pars.py
+++ files/mess_pars.py
@@ -3,6 +3,7 @@ import telebot
 from bs4 import BeautifulSoup
 import datetime
 import time
+from web_parse import parse_schedule_for_day
 
 access_token = ''
 bot = telebot.TeleBot(access_token)
@@ -14,7 +15,7 @@ def message_parse(message):
     message = message.split(' ')
     message[0].upper()
     if message[0] in daynames:       # в этом операторе надо реализовать поиск даты
-        pass
+        parse_schedule_for_day(message[0],message[1], message[2])
     elif message[0] == 'tomorrow':
         pass
     elif message[0] == 'near':
