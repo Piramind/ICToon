089b2566ea3370828fee3ba86d6bb68eaa30c8a7
Lev <tikhonov.lev0@gmail.com>
2020-11-21 23:22:29 +0300

    edit near

--- files/mess_pars.py
+++ files/mess_pars.py
@@ -25,7 +25,44 @@ def message_parse(message):
 
 
     elif message[0] == 'near':
-        pass
+        _, group = message.text.split()
+        current_day = datetime.date.today().weekday()
+        if current_day == 7:
+            current_day = 0
+
+        web_page = get_page(group)
+        not_found = True
+        today = True
+        while not_found:
+            if today:
+                today = False
+                schedule_for_day = parse_schedule_for_day(web_page, daynames[current_day])
+                if schedule_for_day is None:
+                    continue
+                else:
+                    times_lst, locations_lst, lessons_lst = schedule_for_day
+                    now_time = time.strptime(time.strftime('%H:%M'), '%H:%M')
+                    for time_comarison in range(len(times_lst)):
+                        if now_time < time.strptime(times_lst[time_comarison].split('-')[0], '%H:%M'):
+                            bot.send_message(message.chat.id, f'Nearest lesson today at:{times_lst[time_comarison]}\n'
+                                                            f'Lesson: {lessons_lst[time_comarison]} at'
+                                                            f' {locations_lst[time_comarison]}')
+                            not_found = False
+                            break
+            else:
+                current_day += 1
+                if current_day == 7:
+                    current_day = 0
+                schedule_for_day = parse_schedule_for_day(web_page, daynames[current_day])
+                if schedule_for_day is None:
+                    continue
+                else:
+                    times_lst, locations_lst, lessons_lst = schedule_for_day
+                    bot.send_message(message.chat.id, f'Nearest lesson on {daynames[current_day].capitalize()} at:'
+                                                    f'{times_lst[0]}\n'
+                                                    f'Lesson: {lessons_lst[0]} in '
+                                                    f'{locations_lst[0]}')
+                    not_found = False
     elif message[0] == 'дата хз хз':
         pass
     else:
